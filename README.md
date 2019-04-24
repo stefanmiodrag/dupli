9980af3725f3a3e674a6e9d788acc2ec318912e6
Stefan Miodrag <stefanmiodrag@gmail.com>
2019-04-24 22:38:10 +0200

    Initialize Repository ⚡️

---
+++ README.md
@@ -0,0 +1,16 @@
+#dupli 
+
+Module to count the number of duplicate of characters within a string.
+
+## Installation
+
+    $ npm install dupli --save
+
+## Example
+
+```js
+const dupli = require("./index");
+
+const countDuplicates = dupli('abcccAA11ee');
+console.log(countDuplicates); // 4 duplicates were found...
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+
+/*
+Function that will return the count of distinct case-insensitive alphabetic characters
+and numeric digits that occur more than once (both uppercase and lowercase) in the input string. 
+*/
+
+dupli = (str) => {
+    const strArray = str.toLowerCase().split("").sort().join("").match(/(.)\1+/g);
+
+    try {
+        const result = strArray.length;
+        return result + " duplicates were found...";
+
+    } catch (e) {
+        return 0;
+    }
+}
+
+module.exports = dupli;
+
